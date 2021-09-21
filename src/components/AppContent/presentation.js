9207f47750491de9fe20c0e9df0f72d786f59c02
PavelVlach <vlach1989@gmail.com>
2021-09-21 21:54:01 +0200

    dark RetractableWindow

--- src/components/AppContent/presentation.js
+++ src/components/AppContent/presentation.js
@@ -46,37 +46,12 @@ const App = ({onMount, onUnmount, viewLimits}) => {
 				/>
 			</ConnectedMapSet>
 			<RetractableWindow
-				className="worldCereal-FilterWindow"
+				className="worldCereal-FilterWindow ptr-dark"
 				retracted
 				bottomPosition={6}
-				bodyHeight={12}
+				bodyHeight={16}
 				controlBarContent={<ActiveFilterInfo />}
-			>
-				<p>
-					Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui.
-					Aliquam in lorem sit amet leo accumsan lacinia. Nullam sit amet magna
-					in magna gravida vehicula. Etiam posuere lacus quis dolor. Nam sed
-					tellus id magna elementum tincidunt. Integer vulputate sem a nibh
-					rutrum consequat. Donec iaculis gravida nulla. Nulla accumsan, elit
-					sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam
-					nulla vel leo. In rutrum. Donec ipsum massa, ullamcorper in, auctor
-					et, scelerisque sed, est. Nullam justo enim, consectetuer nec,
-					ullamcorper ac, vestibulum in, elit. Donec quis nibh at felis congue
-					commodo.
-				</p>
-				<p>
-					Maecenas ipsum velit, consectetuer eu lobortis ut, dictum at dui.
-					Aliquam in lorem sit amet leo accumsan lacinia. Nullam sit amet magna
-					in magna gravida vehicula. Etiam posuere lacus quis dolor. Nam sed
-					tellus id magna elementum tincidunt. Integer vulputate sem a nibh
-					rutrum consequat. Donec iaculis gravida nulla. Nulla accumsan, elit
-					sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam
-					nulla vel leo. In rutrum. Donec ipsum massa, ullamcorper in, auctor
-					et, scelerisque sed, est. Nullam justo enim, consectetuer nec,
-					ullamcorper ac, vestibulum in, elit. Donec quis nibh at felis congue
-					commodo.
-				</p>
-			</RetractableWindow>
+			></RetractableWindow>
 			<Timeline />
 			{/*<ControlPanel />*/}
 		</div>
