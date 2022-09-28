a918aeae584112795efbb9fa3d3771daf43b7799
PavelVlach <vlach1989@gmail.com>
2022-09-29 00:49:33 +0200

    IntroOverlay: cards styling

--- src/components/Intro/IntroCard/presentation.js
+++ src/components/Intro/IntroCard/presentation.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
-import './style.scss';
 import classNames from 'classnames';
+import {createElement} from 'react';
+import './style.scss';
 
 export const IntroCardTitle = ({children}) => {
 	return <h2 className="worldCereal-IntroCard-title">{children}</h2>;
@@ -18,12 +19,19 @@ IntroCardText.propTypes = {
 	children: PropTypes.node,
 };
 
-const IntroCard = ({disabled, onClick, children}) => {
+const IntroCard = ({disabled, onClick, Icon, children}) => {
 	const classes = classNames('worldCereal-IntroCard', {
 		'is-disabled': disabled,
 	});
 	return (
 		<div className={classes} onClick={onClick}>
+			{Icon
+				? createElement(Icon, {
+						color: disabled ? 'rgb(110, 110, 110)' : 'rgb(210, 160, 20)',
+						set: 'duotone',
+						size: '50',
+				  })
+				: null}
 			{children}
 		</div>
 	);
@@ -32,6 +40,7 @@ const IntroCard = ({disabled, onClick, children}) => {
 IntroCard.propTypes = {
 	disabled: PropTypes.bool,
 	onClick: PropTypes.func,
+	Icon: PropTypes.node,
 	children: PropTypes.node,
 };
 
