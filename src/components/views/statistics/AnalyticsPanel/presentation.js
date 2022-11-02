eb0ef2954741b4b1d152e64229a652a4974c45b7
PavelVlach <vlach1989@gmail.com>
2022-11-02 10:47:08 +0100

    LevelSwitch: UI

--- src/components/views/statistics/AnalyticsPanel/presentation.js
+++ src/components/views/statistics/AnalyticsPanel/presentation.js
@@ -1,8 +1,15 @@
 // import PropTypes from 'prop-types';
+import LevelSwitch from './LevelSwitch';
 import './style.scss';
 
 const AnalyticsPanel = () => {
-	return <div className="worldCereal-AnalyticsPanel"></div>;
+	return (
+		<div className="ptr-dark worldCereal-AnalyticsPanel">
+			<div className="worldCereal-AnalyticsPanel-header">
+				<LevelSwitch />
+			</div>
+		</div>
+	);
 };
 
 AnalyticsPanel.propTypes = {};
