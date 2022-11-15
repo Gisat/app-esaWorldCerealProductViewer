92e9d08fe3d4fc2cbb644bee92d3755dcd0e0f73
PavelVlach <vlach1989@gmail.com>
2022-11-16 00:56:05 +0100

    Charts: add another chart

--- src/data/views/statistics.js
+++ src/data/views/statistics.js
@@ -208,6 +208,67 @@ export default {
 						},
 					},
 				},
+				GlobalShareCountryProductsBarChart: {
+					title: 'Products share',
+					subtitle: 'on total country area [%]',
+					settings: {
+						indexBy: 'id',
+						layout: 'horizontal',
+						margin: {top: 0, right: 25, bottom: 20, left: 100},
+						valueScale: {type: 'linear'},
+						indexScale: {type: 'band', round: true},
+						valueFormat: ' >-,d',
+						axisTop: null,
+						axisRight: null,
+						axisBottom: {
+							values: false,
+							enable: true,
+							// legend: 'Area [ha]',
+							legendPosition: 'middle',
+							legendOffset: 33,
+							tickPadding: 0,
+							tickValues: 4,
+							format: ' >-,d',
+						},
+						// maxValue: 100,
+						padding: 0.1,
+						innerPadding: 3,
+						groupMode: 'grouped',
+						enableGridY: false,
+						enableGridX: true,
+						enableLabel: true,
+						labelSkipHeight: 13,
+						labelTextColor: {from: 'color', modifiers: [['darker', 3]]},
+						theme: {
+							fontSize: 11,
+							textColor: 'var(--base70)',
+							axis: {
+								legend: {
+									text: {
+										fontSize: 13,
+										fontWeight: 'bold',
+									},
+								},
+								ticks: {
+									line: {
+										stroke: 'var(--base20)',
+									},
+								},
+							},
+							grid: {
+								line: {
+									stroke: 'var(--base20)',
+								},
+							},
+							labels: {
+								text: {
+									fontSize: 13,
+									fontWeight: 'bold',
+								},
+							},
+						},
+					},
+				},
 				GlobalCountriesBarChart: {
 					title: 'Product share',
 					subtitle: 'on total country area [%]',
@@ -523,6 +584,28 @@ export default {
 						start: 1,
 						length: 1,
 					},
+					GlobalShareCountryProductsBarChart: {
+						type: 'barChart',
+						options: {
+							nameComponentKey: 'CountryNames',
+							selectedFeaturesOnly: true,
+						},
+						metadataModifiers: {},
+						filterByActive: {
+							application: true,
+							scope: true,
+							areaTreeLevel: true,
+							period: true,
+						},
+						attributeKeys: [
+							'73e3bd86-8701-47e4-b27b-9d2555d68304',
+							'934a9fa2-ddb9-49a8-b4b2-170e39ef531e',
+							'8e71e4da-5a90-4222-9350-f1c1f1bad5c5',
+							'eb9dce86-39c6-480b-b7e3-2fac2101eac0',
+						],
+						start: 1,
+						length: 100,
+					},
 					GlobalCountriesBarChart: {
 						type: 'barChart',
 						options: {
@@ -614,7 +697,10 @@ export default {
 						components: ['GlobalTopTenBarChart', 'GlobalTopTenBarChartShare'],
 					},
 					GlobalOneSelected: {
-						components: ['GlobalSharePieChart'],
+						components: [
+							'GlobalSharePieChart',
+							'GlobalShareCountryProductsBarChart',
+						],
 					},
 					GlobalMultipleSelected: {
 						components: ['GlobalCountriesBarChart'],
