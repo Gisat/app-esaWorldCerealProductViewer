a92372a59bab7748da165fea46d0bb3188521175
PavelVlach <vlach1989@gmail.com>
2022-11-18 23:31:20 +0100

    MapLegend: show attribute name

--- src/components/Maps/presentation.js
+++ src/components/Maps/presentation.js
@@ -54,10 +54,6 @@ const Maps = ({
 					layerKey: 'statistics-global',
 					legend: {
 						component: StyleBasedLegend,
-						props: {
-							title: 'Product area share',
-							unit: '%',
-						},
 					},
 				},
 		  ]
