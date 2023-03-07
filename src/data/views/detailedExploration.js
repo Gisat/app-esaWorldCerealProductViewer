060172909761203378b47bb0cd0b127303d3198a
PavelVlach <vlach1989@gmail.com>
2023-03-07 13:13:11 +0100

    Set Search place as default, hide ZoomToDetail

--- src/data/views/detailedExploration.js
+++ src/data/views/detailedExploration.js
@@ -16,7 +16,6 @@ export default {
 						'detailedExploration-mapSet': {
 							open: [
 								'zoomControls',
-								'zoomToDetail',
 								'zoomToWorld',
 								'layerControls',
 								'addMap',
