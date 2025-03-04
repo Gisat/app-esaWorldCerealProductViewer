0e71afbad9da7d53cc2697655d1a5736546240f4
Krystof <me@krystofvavra.cz>
2025-03-04 05:28:17 +0100

    Add link to processing hub

--- public/static/js/runtimeConfig.js
+++ public/static/js/runtimeConfig.js
@@ -1,3 +1,4 @@
 window.runtimeConfig = {
-    userKey: '3fdd158d-4b78-4d11-92c7-403b4adab4d8' // TODO just for testing, get it dynamically
+    userKey: '3fdd158d-4b78-4d11-92c7-403b4adab4d8', // TODO just for testing, get it dynamically
+		processesAppURL: 'https://worldcerealprocesses-dev.gisat.cz'
 };
